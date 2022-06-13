9d7b39cb4b1cc61bff5a78f8a2b4072536fced55
samitpok <samit.pok@gmail.com>
2022-06-12 19:02:15 -0500

    debug date issue

--- src/main/java/edu/mum/cs544/service/CommentService.java
+++ src/main/java/edu/mum/cs544/service/CommentService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -31,6 +32,8 @@ public class CommentService {
     }
 
     public Comment add(Comment comment) {
+        Date date = new Date();
+        comment.setDate(date);
         return commentRepository.save(comment);
     }
 
